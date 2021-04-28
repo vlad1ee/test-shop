9d7f5f4428773bc5327ec6ad186196f951881cff
vlad <vladlee61@gmail.com>
2021-04-28 19:37:15 +0600

    [#8]Added swagger documentation for API

--- src/shop/settings.py
+++ src/shop/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'orders',
     'mptt',
     'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- src/shop/urls.py
+++ src/shop/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import yasg
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,4 @@ urlpatterns = [
     path('api/v1/', include('orders.urls'))
 ]
 
+urlpatterns += yasg.urlpatterns
